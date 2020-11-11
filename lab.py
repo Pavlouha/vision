9af603581ee9d8baee3c6ca7523902cf81830053
Denis <yourjest@gmail.com>
2020-11-11 16:11:42 +0300

    second

--- lab.py
+++ lab.py
@@ -1,66 +1,59 @@
 import jetson.inference
 import jetson.utils
-from torchvision.models.alexnet import alexnet
 
 import argparse
+import sys
 
 # parse the command line
-parser = argparse.ArgumentParser()
-#parser.add_argument("filename", type=str, help="filename of the image to process")
-#parser.add_argument("--network", type=str, default="googlenet", help="model to use, can be:  googlenet, resnet-18, ect.")
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
+                                 jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
 
+parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
 parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
-args = parser.parse_args()
+parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
 
-# load an image (into shared CPU/GPU memory)
-#img = jetson.utils.loadImage(args.filename)
+is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
 
-input = jetson.utils.videoSource("csi://0")
-output = jetson.utils.videoOutput("display://0")
-font = jetson.utils.cudaFont()
+try:
+	opt = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
 
-# load the recognition network for imagenet
-#net = jetson.inference.imageNet(args.network)
+# load the object detection network
+net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
 
-# for detectNet 
-net = jetson.inference.detectNet(args.network)
-opt="box,labels,conf"
+# create video sources & outputs
+input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
+output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
 
-# classify the image
-#class_idx, confidence = net.Classify(img)
-#class_desc = net.GetClassDesc(class_idx)
-#print("image is recognized as '{:s}' (class #{:d}) with {:f}% confidence".format(class_desc, class_idx, confidence * 100))
-
-#live-streaming
+# process frames until the user exits
 while True:
-    img = input.Capture()
-    
-	# overlay the result on the image - first mode (imagenet)
-    #class_id, confidence = net.Classify(img)
-    #class_desc = net.GetClassDesc(class_id)
-    #font.OverlayText(img, img.width, img.height, "{:05.2f}% {:s}".format(confidence * 100, class_desc), 5, 5, font.White, font.Gray40)
-	
-    # detect objects in the image (with overlay) - second mode
-    detections = net.Detect(img, overlay=opt)
-    # print the detections
-    print("detected {:d} objects in image".format(len(detections)))
-    for detection in detections:
-        print(detection)
+	# capture the next image
+	img = input.Capture()
+
+	# detect objects in the image (with overlay)
+	detections = net.Detect(img, overlay=opt.overlay)
+
+	# print the detections
+	print("detected {:d} objects in image".format(len(detections)))
+
+	for detection in detections:
+		print(detection)
 
 	# render the image
-    output.Render(img)
+	output.Render(img)
 
-	# update the title bar - imagenet
-    #output.SetStatus("{:s} | Network {:.0f} FPS".format(net.GetNetworkName(), net.GetNetworkFPS()))
-
-    # update the title bar - detectnet
-    output.SetStatus("{:s} | Network {:.0f} FPS".format(args.network, net.GetNetworkFPS()))
+	# update the title bar
+	output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
 
 	# print out performance info
-    net.PrintProfilerTimes()
+	net.PrintProfilerTimes()
 
 	# exit on input/output EOS
-    if not input.IsStreaming() or not output.IsStreaming():
-        break
-
-
+	if not input.IsStreaming() or not output.IsStreaming():
+		break
